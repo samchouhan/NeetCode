bc02f671d69d0484b18fe9225ec2b9433c245243
SAKSHAM_CHOUHAN <131700345+samchouhan@users.noreply.github.com>
2026-02-22 14:45:15 +0530

    Update Contains_Duplicate.py

--- Contains_Duplicate.py
+++ Contains_Duplicate.py
@@ -3,6 +3,8 @@ class Solution:
     def hasDuplicate(self, nums: List[int]) -> bool:
         hashset= set()
 #Here we are using hashset method...
+#Storing the array in hashset as defining set() method in python      
+        
         for n in nums:
             if n in hashset:
                 return True
